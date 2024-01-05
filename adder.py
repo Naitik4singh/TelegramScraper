655b90fa66f82faa08eddd7324583b02453c7073
Naitik4singh <153117195+Naitik4singh@users.noreply.github.com>
2024-01-05 09:16:46 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
-# (c) @AbirHasan2005
-# Telegram Group: http://t.me/linux_repo
+# (c) @YS_BOTZ_01
+# Telegram Group: https://t.me/YS_SUPPORT_CHAT
 # Please give me credits if you use any codes from here.
 
 
@@ -9,7 +9,7 @@ print ("░█▀█░█▀▄░█▀▄░█▀▀░█▀▄")
 print ("░█▀█░█░█░█░█░█▀▀░█▀▄")
 print ("░▀░▀░▀▀░░▀▀░░▀▀▀░▀░▀")
 print ("")
-print ("      by \033[1;95m@AbirHasan2005")
+print ("      by \033[1;95m@YS_BOTZ_01")
 print ("\033[1;92m")
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
